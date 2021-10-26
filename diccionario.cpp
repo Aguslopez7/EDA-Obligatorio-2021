4bdfb64ab489aeb2a6e0c3fa35ec88723ad4424f
Aguslopez7 <aln71198@gmail.com>
2021-10-26 16:09:57 -0300

    Modulos Diccionario Fix

--- diccionario.cpp
+++ diccionario.cpp
@@ -14,9 +14,9 @@ struct nodo_abb{
 /*--------------------- Funciones --------------------*/
 
 //12) Agrega una palabra al diccionario.
-/*TipoRetorno IngresarPalabraDiccionario(Cadena palabraAIngresar, dicc ab){
+TipoRetorno IngresarPalabraDiccionario(Cadena palabraAIngresar, dicc ab){
 // Inserta a la palabra en el arbol binario de busqueda.
-	if (ab == NULL){
+	/*if (ab == NULL){
 		ab = new(nodo_abb);
 		ab->dato =x;
 		ab->iz = NULL;
@@ -25,17 +25,30 @@ struct nodo_abb{
 		ab->iz = insertar(x, ab->iz);
 	else
 		ab->de = insertar(x, ab->de);
-	return ab;
-}*/
+	return ab;*/
+	return NO_IMPLEMENTADA;
+}
 
-//13) Borra una palabra del diccionario.
-TipoRetorno BorrarPalabraDiccionario(Cadena palabraABorrar){}
+// 13) Borra una palabra del diccionario.
+TipoRetorno BorrarPalabraDiccionario(Cadena palabraABorrar)
+{
+	return NO_IMPLEMENTADA;
+}
 
-//14) Muestra las palabras del diccionario alfabéticamente.
-TipoRetorno ImprimirDiccionario(){}
+// 14) Muestra las palabras del diccionario alfabéticamente.
+TipoRetorno ImprimirDiccionario()
+{
+	return NO_IMPLEMENTADA;
+}
 
-//15) Muestra las palabras del texto que no se encuentran en el diccionario.
-TipoRetorno ImprimirTextoIncorrecto(){}
+// 15) Muestra las palabras del texto que no se encuentran en el diccionario.
+TipoRetorno ImprimirTextoIncorrecto()
+{
+	return NO_IMPLEMENTADA;
+}
 
-//16) Imprime las últimas palabras ingresadas.
-TipoRetorno ImprimirUltimasPalabras(){}
+// 16) Imprime las últimas palabras ingresadas.
+TipoRetorno ImprimirUltimasPalabras()
+{
+	return NO_IMPLEMENTADA;
+}
